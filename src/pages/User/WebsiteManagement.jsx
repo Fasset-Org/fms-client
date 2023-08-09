8643348b75325db31216934da87362298389f5eb
Kamzen <kamzen1994@gmail.com>
2023-08-09 15:55:20 +0200

    Update WebsiteManagement.jsx

--- src/pages/User/WebsiteManagement.jsx
+++ src/pages/User/WebsiteManagement.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Box, Paper, Stack, Tab, Tabs } from "@mui/material";
+import React from "react";
+import BreadCrumbsHeader from "../../components/BreadCrumbsHeader";
+import { useLocation } from "react-router-dom";
 
 const WebsiteManagement = () => {
-  return (
-    <div>WebsiteManagement</div>
-  )
-}
+  let { pathname } = useLocation();
+  pathname = pathname.slice(1);
+  const [value, setValue] = React.useState(0);
 
-export default WebsiteManagement
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Stack spacing={2}>
+      <BreadCrumbsHeader
+        title="Welcome back Tiyisela Themba Makamu"
+        menus={[
+          "Dashboard",
+          pathname.charAt(0).toUpperCase() + pathname.slice(1)
+        ]}
+        href="/websiteManagement"
+      />
+
+      <Box sx={{ bgcolor: "background.paper" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          sx={{borderRadius: 0, padding: 2}}
+          component={Paper}
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab label="Banners" />
+          <Tab label="Board" />
+          <Tab label="Committees" />
+          <Tab label="Downloads" />
+          <Tab label="Notice Board" />
+          <Tab label="Annual Reports" />
+          <Tab label="Research Reports" />
+        </Tabs>
+      </Box>
+    </Stack>
+  );
+};
+
+export default WebsiteManagement;
