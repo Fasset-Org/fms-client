8bdee2b2e0f7977640d73899b3260bc954f7899c
Kamzen <kamzen1994@gmail.com>
2023-08-09 14:57:58 +0200

    Create BreadCrumbsHeader.jsx

---
+++ src/components/BreadCrumbsHeader.jsx
@@ -0,0 +1,12 @@
+import { Paper, Stack } from '@mui/material'
+import React from 'react'
+
+const BreadCrumbsHeader = () => {
+  return (
+    <Stack component={Paper} height={100}>
+
+    </Stack>
+  )
+}
+
+export default BreadCrumbsHeader
