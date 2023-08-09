4d184ee1de047619122dbe50bc87c0238d3d7115
Kamzen <kamzen1994@gmail.com>
2023-08-09 14:58:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Dashboard from "./pages/User/Dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import LoginUser from "./pages/auth/LoginUser";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { themeLight } from "./fassetTheme";
+import ITUserManagement from "./pages/User/ITUserManagement";
+import WebsiteManagement from "./pages/User/WebsiteManagement";
+import HumanResource from "./pages/User/HumanResource";
+import SupplyChain from "./pages/User/SupplyChain";
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <CssBaseline />
+      <ThemeProvider theme={themeLight}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Navigation />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/userManagement" element={<ITUserManagement />} />
+              <Route path="/websiteManagement" element={<WebsiteManagement />} />
+              <Route path='/humanResource' element={<HumanResource />} />
+              <Route path="/scm" element={<SupplyChain />} />
+            </Route>
+            {/* Auth Routes */}
+            <Route path="/login" element={<LoginUser />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
