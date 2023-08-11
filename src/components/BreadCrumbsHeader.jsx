1b943dae2d66d745b1c1e86349491156a8f5bdd5
Kamzen <kamzen1994@gmail.com>
2023-08-11 15:51:47 +0200

    Update BreadCrumbsHeader.jsx

--- src/components/BreadCrumbsHeader.jsx
+++ src/components/BreadCrumbsHeader.jsx
@@ -9,47 +9,47 @@ import {
 } from "@mui/material";
 import React from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
 
-const BreadCrumbsHeader = ({ title, menus, href }) => {
+const BreadCrumbsHeader = ({ title, menus, ...otherProps }) => {
+  const navigate = useNavigate();
   const BreadLink = styled(Link)({
     textDecoration: "none"
   });
 
-  console.log(menus);
-
   return (
-    <Stack component={Paper} height={100} padding={2} spacing={2}>
+    <Stack
+      component={Paper}
+      height={100}
+      padding={2}
+      spacing={2}
+      {...otherProps}
+    >
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <Typography fontSize={15}>{title}</Typography>
         {menus.length > 1 && (
-          <Button variant="outlined" startIcon={<ArrowBackIcon />}>
+          <Button
+            variant="outlined"
+            startIcon={<ArrowBackIcon />}
+            onClick={() => navigate(-1)}
+          >
             Back
           </Button>
         )}
       </Stack>
       <Stack>
         <Breadcrumbs aria-label="breadcrumb">
-          <BreadLink
-            href="/"
-            sx={{ color: "primary.main", fontWeight: "bolder" }}
-          >
-            Dashboard&nbsp;&nbsp;&nbsp;
-          </BreadLink>
-          {/* <Link
-            underline="hover"
-            color="inherit"
-            href="/material-ui/getting-started/installation/"
-          >
-            Core
-        </Link> */}
-          {menus.length === 2 && (
-            <BreadLink
-              href={href}
-              sx={{ color: "primary.main", fontWeight: "bolder" }}
-            >
-              {menus[1]}&nbsp;&nbsp;&nbsp;
-            </BreadLink>
-          )}
+          {menus.map((menu, i) => {
+            return (
+              <BreadLink
+                href={menu.url}
+                sx={{ color: "primary.main", fontWeight: "bolder" }}
+                key={i}
+              >
+                {menu.name}
+              </BreadLink>
+            );
+          })}
         </Breadcrumbs>
       </Stack>
     </Stack>
