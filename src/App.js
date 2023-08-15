644b109717ec27b3aeac4600b71413160796021b
Kamzen <kamzen1994@gmail.com>
2023-08-15 14:27:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Routes
+} from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import Dashboard from "./pages/User/Dashboard";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -14,6 +18,8 @@ import ResetPassword from "./pages/auth/ResetPassword";
 import Users from "./pages/User/Users";
 import Departments from "./pages/User/Departments";
 import Modules from "./pages/User/Modules";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   const queryClient = new QueryClient();
@@ -23,18 +29,27 @@ function App() {
       <ThemeProvider theme={themeLight}>
         <Router>
           <Routes>
-            <Route path="/" element={<Navigation />}>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/userManagement" element={<ITUserManagement />} />
-              <Route path="/userManagement/users" element={<Users />} />
-              <Route path="/userManagement/departments" element={<Departments />} />
-              <Route path="/userManagement/modules" element={<Modules />} />
-              <Route
-                path="/websiteManagement"
-                element={<WebsiteManagement />}
-              />
-              <Route path="/humanResource" element={<HumanResource />} />
-              <Route path="/scm" element={<SupplyChain />} />
+            <Route path="/" element={<PrivateRoute />}>
+              <Route path="/" element={<Navigation />}>
+                <Route
+                  path="/"
+                  element={<Dashboard />}
+                />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/userManagement" element={<ITUserManagement />} />
+                <Route path="/userManagement/users" element={<Users />} />
+                <Route
+                  path="/userManagement/departments"
+                  element={<Departments />}
+                />
+                <Route path="/userManagement/modules" element={<Modules />} />
+                <Route
+                  path="/websiteManagement"
+                  element={<WebsiteManagement />}
+                />
+                <Route path="/humanResource" element={<HumanResource />} />
+                <Route path="/scm" element={<SupplyChain />} />
+              </Route>
             </Route>
             {/* Auth Routes */}
             <Route path="/login" element={<LoginUser />} />
@@ -43,6 +58,7 @@ function App() {
           </Routes>
         </Router>
       </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
