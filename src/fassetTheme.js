fa0b6e79ec1038f50954c75d17dd637b51c8ed57
Kamzen <kamzen1994@gmail.com>
2023-08-15 21:13:16 +0200

    Update fassetTheme.js

--- src/fassetTheme.js
+++ src/fassetTheme.js
@@ -71,85 +71,13 @@ export const themeLight = createTheme({
           borderRadius: 8
         }
       }
-    },
-    // MuiTab: {
-    //   styleOverrides: {
-    //     root: {
-    //       textTransform: "none"
-    //     }
-    //   }
-    // },
-
-    // MuiTabs: {
-    //   styleOverrides: {
-    //     root: {
-    //       "& .MuiTabs-indicator": {
-    //         display: "none"
-    //       },
-    //       "& .MuiButtonBase-root": {
-    //         justifyContent: "start"
-    //       },
-    //       width: 250
-    //     }
-    //   }
-    // },
-    // MuiAccordionSummary: {
-    //   styleOverrides: {
-    //     root: {
-    //       color: "#FFFFFF"
-    //     }
-    //   }
-    // }
-  },
-  palette: {
-    mode: "light",
-    primary: {
-      light: "#1f2f79",
-      main: "#163683",
-      info: "#333",
-      contrastText: "#ffffff"
-    },
-    secondary: {
-      main: "#a6aa15",
-      contrastText: "#ffffff"
-    },
-    background: {
-      paper: "#fbfbfbff",
-      default: "#f3f3f3ff"
-    }
-  },
-  typography: {
-    fontSize: 12
-  }
-});
-
-export const NeoBrutalism = createTheme({
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        root: {}
-      }
-    },
-    MuiCard: {
-      styleOverrides: {
-        root: {
-          borderRadius: "10px"
-        }
-      }
-    },
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: 8
-        }
-      }
     }
   },
   palette: {
     mode: "light",
     primary: {
       light: "#3a9bfb",
-      main: "#0f4c81",
+      main: "#163683",
       info: "#333",
       contrastText: "#ffffff"
     },
@@ -158,8 +86,131 @@ export const NeoBrutalism = createTheme({
       contrastText: "#ffffff"
     },
     background: {
-      paper: "#50FFAF",
-      default: "#9C7BFF"
+      paper: "#fbfbfbff",
+      default: "#f3f3f3ff"
     }
   }
 });
+
+// export const themeDark = createTheme({
+//   components: {
+//     MuiPaper: {
+//       styleOverrides: {
+//         root: {
+//           border: "1px solid #202020ff",
+//           borderRadius: "10px",
+//         },
+//       },
+//     },
+//     MuiCard: {
+//       styleOverrides: {
+//         root: {
+//           border: "1px solid #202020ff",
+//           borderRadius: "5px",
+//         },
+//       },
+//     },
+//     MuiButton: {
+//       styleOverrides: {
+//         root: {
+//           borderRadius: 8,
+//         },
+//       },
+//     },
+//   },
+//   palette: {
+//     mode: "dark",
+//     primary: {
+//       light: "#3a9bfb",
+//       main: "#163683",
+//       info: "#333",
+//       contrastText: "#ffffff",
+//     },
+//     secondary: {
+//       main: "#0FFFB3",
+//     },
+//     error: {
+//       light: "#e57373",
+//       main: "#f44336",
+//       dark: "#d32f2f",
+//       contrastText: "#333",
+//     },
+//     // background: {
+//     //   // paper: "#444444da",
+//     //   paper: "#202020ff",
+//     //   default: "#191919ff",
+//     // },
+//   },
+// });
+
+// export const themeLight = createTheme({
+//   components: {
+//     MuiPaper: {
+//       styleOverrides: {
+//         root: {}
+//       }
+//     },
+//     MuiCard: {
+//       styleOverrides: {
+//         root: {
+//           borderRadius: "10px"
+//         }
+//       }
+//     },
+//     MuiButton: {
+//       styleOverrides: {
+//         root: {
+//           borderRadius: 8
+//         }
+//       }
+//     },
+//     // MuiTab: {
+//     //   styleOverrides: {
+//     //     root: {
+//     //       textTransform: "none"
+//     //     }
+//     //   }
+//     // },
+
+//     // MuiTabs: {
+//     //   styleOverrides: {
+//     //     root: {
+//     //       "& .MuiTabs-indicator": {
+//     //         display: "none"
+//     //       },
+//     //       "& .MuiButtonBase-root": {
+//     //         justifyContent: "start"
+//     //       },
+//     //       width: 250
+//     //     }
+//     //   }
+//     // },
+//     // MuiAccordionSummary: {
+//     //   styleOverrides: {
+//     //     root: {
+//     //       color: "#FFFFFF"
+//     //     }
+//     //   }
+//     // }
+//   },
+//   palette: {
+//     mode: "light",
+//     primary: {
+//       light: "#1f2f79",
+//       main: "#163683",
+//       info: "#333",
+//       contrastText: "#ffffff"
+//     },
+//     secondary: {
+//       main: "#a6aa15",
+//       contrastText: "#ffffff"
+//     },
+//     background: {
+//       paper: "#fbfbfbff",
+//       default: "#f3f3f3ff"
+//     }
+//   },
+//   typography: {
+//     fontSize: 12
+//   }
+// });
