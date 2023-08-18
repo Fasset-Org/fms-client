946826630dcf69d3131e7e947c2dc65df5571acd
Kamzen <kamzen1994@gmail.com>
2023-08-18 13:53:18 +0200

    Update AddEditUserModal.jsx

--- src/components/Modals/AddEditUserModal.jsx
+++ src/components/Modals/AddEditUserModal.jsx
@@ -13,15 +13,18 @@ import {
   CircularProgress,
   Grid,
   InputLabel,
+  LinearProgress,
   Slide,
   useMediaQuery
 } from "@mui/material";
 import { Form, Formik } from "formik";
 import TextFieldWrapper from "../FormComponents/TextFieldWrapper";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import AdminQuery from "../../stateQueries/Admin";
 import * as Yup from "yup";
 import AlertPopup from "../AlertPopup";
+import SelectFieldWrapper from "../FormComponents/SelectFieldWrapper";
+import AuthQuery from "../../stateQueries/Auth";
 
 function BootstrapDialogTitle(props) {
   const { children, onClose, ...other } = props;
@@ -58,8 +61,25 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
 const AddEditUserModal = () => {
   const [open, setOpen] = React.useState(false);
+  let departments = [];
+  let modules = [];
   const theme = useTheme();
   const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const userTypes = [
+    {
+      value: "Super",
+      label: "Super"
+    },
+    {
+      value: "Admin",
+      label: "Admin"
+    },
+    {
+      value: "User",
+      label: "User"
+    }
+  ];
 
   const queryClient = useQueryClient();
   const adminQuery = useMutation({
@@ -75,9 +95,72 @@ const AddEditUserModal = () => {
     }
   });
 
+  const { data: userInfo } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: async () => {
+      return await AuthQuery.isUserLoggedIn();
+    }
+  });
+
+  const { data: departmentQuery, isLoading: departmentFetchLoading } = useQuery(
+    {
+      queryKey: ["departments"],
+      queryFn: async () => {
+        return await AdminQuery.getAllDepartments();
+      }
+    }
+  );
+
+  const { data: moduleQuery, isLoading: moduleFetchLoading } = useQuery({
+    queryKey: ["modules"],
+    queryFn: async () => {
+      return await AdminQuery.getAllModules();
+    }
+  });
+
+  if (departmentQuery?.departments) {
+    departments = [
+      ...departmentQuery?.departments?.map((department) => {
+        return {
+          value: department.id,
+          label: department.departmentName
+        };
+      })
+    ];
+  }
+
+  if (moduleQuery?.modules) {
+    modules = [
+      ...moduleQuery?.modules?.map((module) => {
+        return {
+          value: module.id,
+          label: module.moduleName
+        };
+      })
+    ];
+  }
+
+  const addUserQuery = useMutation({
+    mutationFn: async (formData) => {
+      return await AdminQuery.addUser(formData);
+    },
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (err) => {
+      console.log(err);
+    }
+  });
+
   const handleClose = () => {
     setOpen(false);
   };
+
+  if (departmentFetchLoading || moduleFetchLoading) {
+    return <LinearProgress />;
+  }
+
+  console.log(userInfo);
 
   return (
     <>
@@ -104,30 +187,65 @@ const AddEditUserModal = () => {
         <DialogContent dividers>
           <Formik
             initialValues={{
+              userId: userInfo?.user?.id,
               email: "",
-              moduleDesc: ""
+              userType: "",
+              departmentId: "",
+              moduleId: ""
             }}
             validationSchema={Yup.object().shape({
-              moduleName: Yup.string().required("Module name required"),
-              moduleDesc: Yup.string().required("Module description required")
+              email: Yup.string()
+                .required("Email required")
+                .test(
+                  "email",
+                  "Please provide valid fasset email",
+                  function (email) {
+                    if (email.split("@")[1] !== "fasset.org.za") {
+                      return false;
+                    }
+
+                    return true;
+                  }
+                ),
+              userType: Yup.string().required("User type required"),
+              departmentId: Yup.string().required("Department required"),
+              moduleId: Yup.string().required("Module required")
             })}
             onSubmit={(values) => {
-              adminQuery.mutate(values);
+              addUserQuery.mutate(values);
             }}
+            enableReinitialize={true}
           >
             {() => {
               return (
                 <Form>
                   <Grid container spacing={2}>
                     <Grid item xs={12} md={12}>
-                      <InputLabel sx={{ mb: 1 }}>Module Name</InputLabel>
-                      <TextFieldWrapper name="moduleName" label="Module Name" />
+                      <InputLabel sx={{ mb: 1 }}>Email</InputLabel>
+                      <TextFieldWrapper name="email" label="Email" />
                     </Grid>
                     <Grid item xs={12} md={12}>
-                      <InputLabel sx={{ mb: 1 }}>Module Description</InputLabel>
-                      <TextFieldWrapper
-                        name="moduleDesc"
-                        label="Module Description"
+                      <InputLabel sx={{ mb: 1 }}>User Type</InputLabel>
+                      <SelectFieldWrapper
+                        name="userType"
+                        label="User Type"
+                        options={userTypes}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <InputLabel sx={{ mb: 1 }}>Department</InputLabel>
+                      <SelectFieldWrapper
+                        name="departmentId"
+                        label="Department"
+                        options={departments}
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <InputLabel sx={{ mb: 1 }}>Module</InputLabel>
+                      <SelectFieldWrapper
+                        name="moduleId"
+                        label="Module"
+                        options={modules}
                       />
                     </Grid>
                     <Grid item xs={12} md={12}>
