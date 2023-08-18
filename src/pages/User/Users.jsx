64fb636e9bc52a33b0e3b26b87813daed8755dda
Kamzen <kamzen1994@gmail.com>
2023-08-18 13:53:03 +0200

    Update Users.jsx

--- src/pages/User/Users.jsx
+++ src/pages/User/Users.jsx
@@ -1,8 +1,10 @@
 import { Stack } from "@mui/material";
 import React from "react";
-import AddEditUserModal from "../../components/Modals/AddEditModuleModal";
+import AddEditUserModal from "../../components/Modals/AddEditUserModal";
+
 
 const Users = () => {
+  
   return (
     <Stack justifyContent="center" alignItems="center">
       <AddEditUserModal />
