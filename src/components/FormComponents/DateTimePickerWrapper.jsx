df9c56de286dc0ff43e7a46087fb19cc58cfb059
Kamzen <kamzen1994@gmail.com>
2023-08-23 19:49:24 +0200

    Update DateTimePickerWrapper.jsx

--- src/components/FormComponents/DateTimePickerWrapper.jsx
+++ src/components/FormComponents/DateTimePickerWrapper.jsx
@@ -4,14 +4,11 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { FormHelperText } from "@mui/material";
 
 const DateTimePickerWrapper = ({ name, ...otherProps }) => {
   const [field, meta] = useField(name);
   const { setFieldValue } = useFormikContext();
-
-  const handleChange = (date) => {
-    setFieldValue(name, date);
-  };
 
   const configDateTimePicker = {
     ...field,
@@ -20,11 +17,10 @@ const DateTimePickerWrapper = ({ name, ...otherProps }) => {
     fullWidth: true,
     InputLabelProps: {
       shrink: true
-    },
-    onChange: handleChange
+    }
   };
 
-  if (meta && meta.touched && meta.error) {
+  if (meta.touched && meta.error) {
     configDateTimePicker.error = true;
     configDateTimePicker.helperText = meta.error;
   }
@@ -32,8 +28,14 @@ const DateTimePickerWrapper = ({ name, ...otherProps }) => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={["DateTimePicker"]}>
-        <DateTimePicker {...configDateTimePicker} />
+        <DateTimePicker
+          {...configDateTimePicker}
+          onChange={(date) => setFieldValue(name, date)}
+        />
       </DemoContainer>
+      {meta?.touched && meta?.error && (
+        <FormHelperText error>{meta?.error}</FormHelperText>
+      )}
     </LocalizationProvider>
   );
 };
