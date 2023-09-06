2a0f2394b6cc65b31feedf350dc09f10f1701696
Kamzen <kamzen1994@gmail.com>
2023-09-06 14:44:53 +0200

    Update AddEditPosition.jsx

--- src/pages/User/HumanResource/AddEditPosition.jsx
+++ src/pages/User/HumanResource/AddEditPosition.jsx
@@ -1,18 +1,39 @@
-import { Box, Button, Grid, Stack, Typography } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Grid,
+  InputLabel,
+  Stack,
+  Typography
+} from "@mui/material";
 import React, { useState } from "react";
 import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
-import { Form, Formik } from "formik";
+import { Field, Form, Formik } from "formik";
 import TextFieldWrapper from "../../../components/FormComponents/TextFieldWrapper";
 import TextAreaFieldWrapper from "../../../components/FormComponents/TextAreaFieldWrapper";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import AdminQuery from "../../../stateQueries/Admin";
 import SelectFieldWrapper from "../../../components/FormComponents/SelectFieldWrapper";
 import AddPositionQuestion from "../../../components/Modals/AddPositionQuestion";
 import AddQualificationModal from "../../../components/Modals/AddQualificationModal";
+import UserQuery from "../../../stateQueries/User";
+import Checkbox from "@mui/material/Checkbox";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import * as Yup from "yup";
 
 const AddEditPosition = () => {
   const [isQualificationRequired, setIsQualificationRequired] = useState(false);
   let departments = [];
+  let qualifications = [];
+  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+  const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+  const queryClient = useQueryClient();
+
   const departmentQuery = useQuery({
     queryKey: ["departments"],
     queryFn: async () => {
@@ -20,9 +41,39 @@ const AddEditPosition = () => {
     }
   });
 
-  if (departmentQuery?.departments) {
+  const { data } = useQuery({
+    queryKey: ["qualifications"],
+    queryFn: async () => {
+      return await UserQuery.HumanResourceQuery.getAllQualification();
+    },
+    onSuccess: (data) => {
+      // queryClient.invalidateQueries("qualifications");
+    }
+  });
+
+  const addPositionMutation = useMutation({
+    mutationFn: async (formData) => {
+      return await UserQuery.HumanResourceQuery.addPosition(formData);
+    },
+    onSuccess: (data) => {
+      queryClient.setQueriesData(["position"], data?.position);
+    }
+  });
+
+  if (data?.qualifications) {
+    qualifications = [
+      ...data?.qualifications?.map((qualification) => {
+        return {
+          value: qualification.id,
+          label: qualification.qualificationName
+        };
+      })
+    ];
+  }
+
+  if (departmentQuery?.data?.departments) {
     departments = [
-      ...departmentQuery?.departments?.map((department) => {
+      ...departmentQuery?.data?.departments?.map((department) => {
         return {
           value: department.id,
           label: department.departmentName
@@ -47,15 +98,43 @@ const AddEditPosition = () => {
           <Typography fontSize={20} fontWeight="bolder" textAlign="center">
             Add Position
           </Typography>
+
           <Formik
             initialValues={{
               jobTitle: "",
               purposeOfJob: "",
-              departmentId: ""
+              departmentId: "",
+              reportingTo: "",
+              remuneration: "",
+              applicationsEmail: "",
+              jobSpecDocumentName: "",
+              qualifications: []
+            }}
+            validationSchema={Yup.object().shape({
+              jobTitle: Yup.string().required("Job title required"),
+              purposeOfJob: Yup.string().required("Purpose of job required"),
+              departmentId: Yup.string().required("Please select deparment"),
+              reportingTo: Yup.string().required("Line manager required"),
+              applicationsEmail: Yup.string()
+                .email("Invalid email format")
+                .required("Email required"),
+              jobSpecDocumentName: Yup.string().required(
+                "Please upload the job description document"
+              )
+            })}
+            onSubmit={(values) => {
+              const formData = new FormData();
+              for (const [key, value] of Object.entries(values)) {
+                if (key === "qualifications")
+                  formData.append(key, JSON.stringify([...value]));
+                else formData.append(key, value);
+              }
+
+              addPositionMutation.mutate(formData);
             }}
           >
-            {({ values }) => {
-              console.log(values);
+            {({ values, setFieldValue }) => {
+              // console.log(values);
               return (
                 <Form>
                   <Grid container spacing={2}>
@@ -70,23 +149,15 @@ const AddEditPosition = () => {
                     </Grid>
                     <Grid item xs={12} md={12}>
                       <SelectFieldWrapper
-                        name="roleId"
-                        label="Position"
-                        options={departments}
-                      />
-                    </Grid>
-                    <Grid item xs={12} md={12}>
-                      <SelectFieldWrapper
                         name="departmentId"
                         label="Department"
                         options={departments}
                       />
                     </Grid>
                     <Grid item xs={12} md={12}>
-                      <SelectFieldWrapper
-                        name="lineRoleId"
+                      <TextFieldWrapper
+                        name="reportingTo"
                         label="Reporting To"
-                        options={departments}
                       />
                     </Grid>
                     <Grid item xs={12} md={12}>
@@ -96,10 +167,47 @@ const AddEditPosition = () => {
                       />
                     </Grid>
                     <Grid item xs={12} md={12}>
+                      <InputLabel sx={{ color: "warning.main", fontSize: 12 }}>
+                        If you don't have an email, please ask IT department to
+                        create one for you before uploading the position.
+                      </InputLabel>
+                      <TextFieldWrapper
+                        name="applicationsEmail"
+                        label="Email To Receive Applications"
+                        type="email"
+                      />
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <Field name="jobSpecDocumentName">
+                        {({ field, form, meta }) => (
+                          <TextField
+                            type="file"
+                            label="Position Job Description"
+                            InputLabelProps={{
+                              shrink: true
+                            }}
+                            error={meta.touched && meta.error}
+                            helperText={
+                              meta.touched && meta.error && meta.error
+                            }
+                            fullWidth
+                            onChange={(event) => {
+                              form.setFieldValue(
+                                field.name,
+                                event.currentTarget.files[0]
+                              );
+                            }}
+                          />
+                        )}
+                      </Field>
+                    </Grid>
+                    <Grid item xs={12} md={12}>
+                      <InputLabel>Is Any Qualification Required?</InputLabel>
                       <Stack
                         direction="row"
                         justifyContent="space-between"
                         alignItems="center"
+                        mt={2}
                       >
                         <Box>
                           <Button
@@ -116,17 +224,78 @@ const AddEditPosition = () => {
                             No
                           </Button>
                         </Box>
-                        {isQualificationRequired && <AddQualificationModal />}
+                        {isQualificationRequired && (
+                          <Stack
+                            direction="row"
+                            spacing={2}
+                            alignItems="center"
+                          >
+                            <Typography fontSize={12} fontWeight="bolder">
+                              Don't find qualification you looking for?
+                            </Typography>
+                            <AddQualificationModal />
+                          </Stack>
+                        )}
                       </Stack>
                     </Grid>
+
                     {isQualificationRequired && (
                       <Grid item xs={12} md={12}>
-                        <TextFieldWrapper
-                          name="qualificationName"
-                          label="Qualification"
-                        />
+                        {qualifications.length > 0 ? (
+                          <Autocomplete
+                            multiple
+                            id="checkboxes-tags-demo"
+                            options={
+                              qualifications.length > 0 && qualifications
+                            }
+                            onChange={(e, value) => {
+                              setFieldValue("qualifications", value);
+                            }}
+                            disableCloseOnSelect
+                            getOptionLabel={(option) => option.label}
+                            renderOption={(props, option, { selected }) => {
+                              return (
+                                <li {...props} key={option.id}>
+                                  <Checkbox
+                                    icon={icon}
+                                    checkedIcon={checkedIcon}
+                                    style={{ marginRight: 8 }}
+                                    checked={selected}
+                                  />
+                                  {option.label}
+                                </li>
+                              );
+                            }}
+                            renderInput={(params) => (
+                              <TextField
+                                {...params}
+                                label="Qualification"
+                                placeholder="Qualification"
+                              />
+                            )}
+                          />
+                        ) : (
+                          <Alert variant="standard" color="info">
+                            <Typography>
+                              No Qualification Available Please Add
+                              Qualifications To Select From
+                            </Typography>
+                          </Alert>
+                        )}
                       </Grid>
                     )}
+
+                    <Grid item xs={12} md={12}>
+                      <Box textAlign="end">
+                        <Button
+                          type="submit"
+                          variant="contained"
+                          sx={{ width: 200 }}
+                        >
+                          Submit
+                        </Button>
+                      </Box>
+                    </Grid>
                     <Grid item xs={12} md={12}>
                       <Box textAlign="end">
                         <AddPositionQuestion />
