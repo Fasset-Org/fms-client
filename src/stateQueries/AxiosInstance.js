fb9ce4b72b85325bec1a115eab95c32db294d592
Kamzen <kamzen1994@gmail.com>
2023-09-28 08:33:03 +0200

    Update AxiosInstance.js

--- src/stateQueries/AxiosInstance.js
+++ src/stateQueries/AxiosInstance.js
@@ -3,10 +3,8 @@ import axios from "axios";
 
 const authToken = localStorage.getItem('authToken');
 
-console.log(authToken)
-
 const axiosInstance = axios.create({
-  baseURL: "http://102.37.217.58:5000/api/dev",
+  baseURL: "http://localhost:5000/api/dev",
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${authToken}`
