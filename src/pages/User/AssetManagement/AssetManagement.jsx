f7f3c7d7e62ec4e4a64397bfe8188b8a2b306cde
SiyamthandaNyathi <131876850+SiyamthandaNyathi@users.noreply.github.com>
2023-11-08 11:11:08 +0200

    Create AssetManagement.jsx

---
+++ src/pages/User/AssetManagement/AssetManagement.jsx
@@ -0,0 +1,76 @@
+import { Grid, Stack } from "@mui/material";
+import React from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
+import DashboardCard from "../../../components/DashboardCard";
+import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
+import FolderIcon from '@mui/icons-material/Folder';
+import PanToolAltIcon from '@mui/icons-material/PanToolAlt';
+
+
+
+const AssetManagement = () => {
+  const navigate = useNavigate();
+  const [open] = useOutletContext();
+  const menuList = [
+    {
+      title: "Devices",
+      icon: LaptopChromebookIcon,
+      url: "/assetManagement/devices"
+    },
+    {
+      title: "License/Subcription",
+      icon: PanToolAltIcon,
+      url: "/assetManagement/licensesubcription"
+    },
+    {
+      title: "Simcards/Cellphone",
+      icon: PhoneIphoneIcon,
+      url: "/assetManagement/simcardscellphone"
+    },
+    
+    {
+      title: "Disposal",
+      icon: DeleteForeverIcon,
+      url: "/assetManagement/disposal"
+    },
+    {
+      title: "Reports",
+      icon: FolderIcon,
+      url: "/assetManagement/reports"
+    },
+
+    
+  ];
+  return (
+    <Stack>
+      <BreadCrumbsHeader
+        title="Welcome back Tiyisela Themba Makamu"
+        menus={[
+          { name: "Dashboard", url: "/dashboard" },
+          { name: "Asset Management", url: "/assetmanagement" }
+        ]}
+        sx={{ mb: 2 }}
+      />
+      <Stack sx={{ width: "100%" }} justifyContent="center" alignItems="center">
+        <Grid container spacing={2}>
+          {menuList.map((menu, i) => {
+            return (
+              <Grid item xs={12} md={open ? 4 : 3} key={i}>
+                <DashboardCard
+                  title={menu.title}
+                  Icon={menu.icon}
+                  onClick={() => navigate(menu.url)}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default AssetManagement;
