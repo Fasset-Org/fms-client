a5117c7e5a13a0f6955411613d915b24d00bc3d3
Kamzen <kamzen1994@gmail.com>
2023-12-12 10:17:33 +0200

    Create ImageCrop.jsx

---
+++ src/components/ImageCrop.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "react-image-crop/dist/ReactCrop.css";
+import src from "../assets/images/blueLogo-transparentBg.png";
+
+const ImageCrop = () => {
+  return <div></div>;
+};
+
+export default ImageCrop;
