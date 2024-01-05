9266d322122f4d128325a4814dab6fd7301dc19d
Kamzen <kamzen1994@gmail.com>
2024-01-05 10:05:37 +0200

    Update Banners.jsx

--- src/pages/User/CSE/Banners.jsx
+++ src/pages/User/CSE/Banners.jsx
@@ -1,21 +1,37 @@
-import { Stack } from '@mui/material'
-import React from 'react'
-import BreadCrumbsHeader from '../../../components/BreadCrumbsHeader'
+import { Stack } from "@mui/material";
+import React, { useState } from "react";
+import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
+import UploadBannerButton from "../../../components/crop/utils/UploadBannerButton";
+import ImageCropModal from "../../../components/crop/ImageCropModal";
 
 const Banners = () => {
+  const [photoURL, setPhotoURL] = React.useState("");
+  const [cropOpen, setCropOpen] = useState(false);
+
   return (
     <Stack spacing={2}>
-    <BreadCrumbsHeader
-      title="Welcome back Tiyisela Themba Makamu"
-      menus={[
-        { name: "Dashboard", url: "/dashboard" },
-        { name: "Website Management", url: "/websiteManagement" },
-        { name: "Banners", url: "/cse/banners" }
-      ]}
-      sx={{ mb: 2 }}
-    />
+      <BreadCrumbsHeader
+        title="Welcome back Tiyisela Themba Makamu"
+        menus={[
+          { name: "Dashboard", url: "/dashboard" },
+          { name: "Website Management", url: "/websiteManagement" },
+          { name: "Banners", url: "/cse/banners" }
+        ]}
+        sx={{ mb: 2 }}
+      />
+      <Stack justifyContent="center" alignItems="center">
+        <UploadBannerButton setPhotoURL={setPhotoURL} setCropOpen={setCropOpen} />
+        {cropOpen && (
+          <ImageCropModal
+            photoURL={photoURL}
+            cropOpen={cropOpen}
+            setCropOpen={setCropOpen}
+            setPhotoURL={setPhotoURL}
+          />
+        )}
       </Stack>
-  )
-}
+    </Stack>
+  );
+};
 
-export default Banners
+export default Banners;
