1910fdb780eb1eef25ac1eba20359cf01ca7bc8f
Kamzen <kamzen1994@gmail.com>
2024-01-05 10:06:08 +0200

    Create UploadBannerButton.jsx

---
+++ src/components/crop/utils/UploadBannerButton.jsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1
+});
+
+export default function UploadBannerButton({ setPhotoURL, setCropOpen }) {
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      setPhotoURL(URL.createObjectURL(file));
+      setCropOpen(true);
+    }
+  };
+
+  return (
+    <Button component="label" variant="contained" startIcon={<AddAPhotoIcon />}>
+      Upload Banner
+      <VisuallyHiddenInput type="file" onChange={handleChange} />
+    </Button>
+  );
+}
