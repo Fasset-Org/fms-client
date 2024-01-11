e3319c830ca9036196e1a9b204738c2689c0f324
Kamzen <kamzen1994@gmail.com>
2024-01-11 08:56:43 +0200

    Update Board.jsx

--- src/pages/User/CSE/Board.jsx
+++ src/pages/User/CSE/Board.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import React from "react";
+import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
+import AddEditBoardMemberModal from "../../../components/Modals/AddEditBoardMemberModal";
 
 const Board = () => {
   return (
-    <div>Board</div>
-  )
-}
+    <Stack spacing={2}>
+      <BreadCrumbsHeader
+        title="Welcome back Tiyisela Themba Makamu"
+        menus={[
+          { name: "Dashboard", url: "/dashboard" },
+          { name: "Website Management", url: "/websiteManagement" },
+          { name: "Board", url: "/cse/board" }
+        ]}
+        sx={{ mb: 2 }}
+      />
 
-export default Board
+      <Stack alignItems="center">
+        <AddEditBoardMemberModal />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Board;
