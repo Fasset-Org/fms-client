09f88608665596ee3b643e825010fd8a1c3d7c72
Kamzen <kamzen1994@gmail.com>
2024-01-11 08:57:23 +0200

    Update AnnualReports.jsx

--- src/pages/User/CSE/AnnualReports.jsx
+++ src/pages/User/CSE/AnnualReports.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import React from "react";
+import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
+import AddEditAnnualReportsModal from "../../../components/Modals/AddEditAnnualReportsModal";
 
 const AnnualReports = () => {
   return (
-    <div>AnnualReports</div>
-  )
-}
+    <Stack spacing={2}>
+      <BreadCrumbsHeader
+        title="Welcome back Tiyisela Themba Makamu"
+        menus={[
+          { name: "Dashboard", url: "/dashboard" },
+          { name: "Website Management", url: "/websiteManagement" },
+          { name: "Annual Reports", url: "/cse/annualReports" }
+        ]}
+        sx={{ mb: 2 }}
+      />
 
-export default AnnualReports
+      <Stack alignItems="center">
+        <AddEditAnnualReportsModal />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default AnnualReports;
