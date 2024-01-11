017f526ec4d7049ef4cdf47fed035ef4b30bf14d
Kamzen <kamzen1994@gmail.com>
2024-01-11 08:56:10 +0200

    Update ResearchReports.jsx

--- src/pages/User/CSE/ResearchReports.jsx
+++ src/pages/User/CSE/ResearchReports.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
+import AddEditResearchReportsModal from "../../../components/Modals/AddEditResearchReportsModal";
+import { Stack } from "@mui/material";
 
 const ResearchReports = () => {
   return (
-    <div>ResearchReports</div>
-  )
-}
+    <Stack spacing={2}>
+      <BreadCrumbsHeader
+        title="Welcome back Tiyisela Themba Makamu"
+        menus={[
+          { name: "Dashboard", url: "/dashboard" },
+          { name: "Website Management", url: "/websiteManagement" },
+          { name: "Research Reports", url: "/cse/researchReports" }
+        ]}
+        sx={{ mb: 2 }}
+      />
 
-export default ResearchReports
+      <Stack alignItems="center">
+        <AddEditResearchReportsModal />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default ResearchReports;
