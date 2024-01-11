0fc390f8950448a315178bf84f4f459b7ba5da4b
Kamzen <kamzen1994@gmail.com>
2024-01-11 08:56:27 +0200

    Update Committees.jsx

--- src/pages/User/CSE/Committees.jsx
+++ src/pages/User/CSE/Committees.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import React from "react";
+import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
+import AddEditCommitteeMemberModal from "../../../components/Modals/AddEditCommitteeMemberModal";
 
 const Committees = () => {
   return (
-    <div>Committees</div>
-  )
-}
+    <Stack spacing={2}>
+      <BreadCrumbsHeader
+        title="Welcome back Tiyisela Themba Makamu"
+        menus={[
+          { name: "Dashboard", url: "/dashboard" },
+          { name: "Website Management", url: "/websiteManagement" },
+          { name: "Committees", url: "/cse/committees" }
+        ]}
+        sx={{ mb: 2 }}
+      />
 
-export default Committees
+      <Stack alignItems="center">
+        <AddEditCommitteeMemberModal />
+      </Stack>
+    </Stack>
+  );
+};
+
+export default Committees;
