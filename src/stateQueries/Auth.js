c0d020b62260ac69050b6c4e3a2901b32a381cfe
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-03-18 10:47:32 +0200

    Update Auth.js

--- src/stateQueries/Auth.js
+++ src/stateQueries/Auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import axiosInstance from "./AxiosInstance";
-axios.defaults.baseURL = "http://102.37.217.58:5000/api/dev";
-// axios.defaults.baseURL="http://localhost:8001/api/dev"
+// axios.defaults.baseURL = "http://102.37.217.58:5000/api/dev";
+axios.defaults.baseURL = `${process.env.REACT_APP_API_URL}`;
 const AuthQuery = {
   loginUser: async (formData) => {
     const resp = await axios.post(`/auth/login`, formData);
