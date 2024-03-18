116173b173a1cef1a361f5b87bc0ab638a0fa82e
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-03-18 10:47:29 +0200

    Update AxiosInstance.js

--- src/stateQueries/AxiosInstance.js
+++ src/stateQueries/AxiosInstance.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-
-const authToken = localStorage.getItem('authToken');
+const authToken = localStorage.getItem("authToken");
 
 const axiosInstance = axios.create({
-  baseURL: "http://102.37.217.58:5000/api/dev",
-  // baseURL: "http://localhost:8001/api/dev",
+  // baseURL: "http://102.37.217.58:5000/api/dev",
+  baseURL: `${process.env.REACT_APP_API_URL}`,
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${authToken}`
   }
-})
+});
 
 export default axiosInstance;
