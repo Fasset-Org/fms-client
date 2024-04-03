3a3113bb3f26a2d04a52bbb63f5b4cfb85f2a1f2
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-04-03 06:45:18 +0200

    Update JobApplications.jsx

--- src/pages/User/HumanResource/JobApplications.jsx
+++ src/pages/User/HumanResource/JobApplications.jsx
@@ -1,15 +1,5 @@
-import {
-  Box,
-  Button,
-  Chip,
-  FormControl,
-  InputLabel,
-  LinearProgress,
-  MenuItem,
-  Select,
-  Stack
-} from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Box, Button, Chip, LinearProgress, Stack } from "@mui/material";
+import React from "react";
 import BreadCrumbsHeader from "../../../components/BreadCrumbsHeader";
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
@@ -24,40 +14,28 @@ const JobApplications = () => {
   const { positionId } = useParams();
   const navigate = useNavigate();
 
-  // const [page, setPage] = React.useState(0);
-  // const [rowsPerPage, setRowsPerPage] = React.useState(5);
-  const [applications, setApplications] = useState([]);
-  const [selectValue, setSelectValue] = useState("");
+  // const [selectValue, setSelectValue] = useState("");
 
-  // const handleChangePage = (event, newPage) => {
-  //   setPage(newPage);
-  // };
+  // const filterOptions = [
+  //   {
+  //     value: "",
+  //     label: "None"
+  //   },
+  //   {
+  //     value: "submitted",
+  //     label: "Submitted"
+  //   },
+  //   {
+  //     value: "shortlisted",
+  //     label: "ShortListed"
+  //   },
+  //   {
+  //     value: "rejected",
+  //     label: "Rejected"
+  //   }
+  // ];
 
-  // const handleChangeRowsPerPage = (event) => {
-  //   setRowsPerPage(parseInt(event.target.value, 10));
-  //   setPage(0);
-  // };
-
-  const filterOptions = [
-    {
-      value: "",
-      label: "None"
-    },
-    {
-      value: "submitted",
-      label: "Submitted"
-    },
-    {
-      value: "shortlisted",
-      label: "ShortListed"
-    },
-    {
-      value: "rejected",
-      label: "Rejected"
-    }
-  ];
-
-  const { data, isLoading, isError } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["applications"],
     queryFn: async () => {
       return await UserQuery.HumanResourceQuery.getPositionApplications(
@@ -67,8 +45,6 @@ const JobApplications = () => {
     enabled: !!positionId
   });
 
-  console.log(data?.applications);
-
   const { data: applicationData, isLoading: loading } = useQuery({
     queryKey: ["position"],
     queryFn: async () => {
@@ -77,9 +53,30 @@ const JobApplications = () => {
     enabled: !!positionId
   });
 
-  console.log(applicationData?.position?.PositionQuestions[0]?.question);
+  const getApplicationQuestion = (id) => {
+    const question = applicationData?.position?.PositionQuestions?.find(
+      (positionQuestion) => {
+        return positionQuestion.id === id;
+      }
+    );
+
+    return question?.question;
+  };
+
+  const compareAnswer = (answer, id) => {
+    const question = applicationData?.position?.PositionQuestions?.find(
+      (positionQuestion) => {
+        return positionQuestion.id === id;
+      }
+    );
+
+    // console.log(question)
+
+    return answer >= question?.answer || answer === question?.answer ? true : false;
+  };
 
   let positionQuestions = [];
+  let applications = [];
 
   if (applicationData?.position?.PositionQuestions?.length > 0) {
     for (const question of applicationData?.position?.PositionQuestions) {
@@ -89,7 +86,23 @@ const JobApplications = () => {
         editable: true
       });
     }
+
+    if (data?.applications?.length > 0) {
+      for (const application of data?.applications) {
+        let positionQuestionAnswers = {};
+
+        for (const answer of application?.ApplicationAnswers) {
+          Object.assign(positionQuestionAnswers, {
+            [getApplicationQuestion(answer.positionQuestionId)]: answer.answer
+          });
+        }
+
+        applications.push({ ...application, ...positionQuestionAnswers });
+      }
+    }
   }
+
+  console.log(applications);
 
   const ActionButtons = ({ params }) => {
     return (
@@ -98,6 +111,7 @@ const JobApplications = () => {
         spacing={2}
         // border={1}
         justifyContent="center"
+        // border={1}
       >
         <Button
           variant="outlined"
@@ -119,24 +133,32 @@ const JobApplications = () => {
   const columns = [
     { field: "idNumber", headerName: "ID" },
     {
-      field: "fullname",
-      headerName: "Full Name",
+      field: "firstName",
+      headerName: "First Name",
+      editable: true
+    },
+    {
+      field: "lastName",
+      headerName: "Last Name",
       editable: true
     },
     {
       field: "email",
       headerName: "Email",
-      editable: true
+      editable: true,
+      hide: true
     },
     {
       field: "gender",
       headerName: "Gender",
-      editable: true
+      editable: true,
+      hide: true
     },
     {
       field: "nationality",
       headerName: "Nationality",
-      editable: true
+      editable: true,
+      hide: true
     },
     ...positionQuestions,
     {
@@ -166,26 +188,12 @@ const JobApplications = () => {
       // field: "action",
       headerName: "Action",
       sortable: false,
+      width: 260,
+      headerAlign: "center",
+      cellAlign: 'center',
       renderCell: (params) => <ActionButtons params={params} />
     }
   ];
-
-  const handleFilter = (filterValue) => {
-    setApplications(data?.applications);
-    if (filterValue !== "") {
-      setApplications((prevApplications) => {
-        return prevApplications.filter(
-          (option) => option.status === filterValue
-        );
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (!isLoading && !isError) {
-      setApplications(data?.applications);
-    }
-  }, [data?.applications, isError, isLoading]);
 
   if (isLoading || loading) {
     return <LinearProgress />;
@@ -204,7 +212,7 @@ const JobApplications = () => {
         sx={{ mb: 2, width: "100%" }}
       />
       <Stack direction="row" width="100%" alignItems="center" spacing={2}>
-        <Box width="80%">
+        {/* <Box width="80%">
           <FormControl fullWidth>
             <InputLabel id="select">Select Filter</InputLabel>
             <Select
@@ -215,7 +223,7 @@ const JobApplications = () => {
               value={selectValue}
               onChange={(e) => {
                 setSelectValue(e.target.value);
-                handleFilter(e.target.value);
+                // handleFilter(e.target.value);
               }}
             >
               {filterOptions.map((item, index) => {
@@ -227,14 +235,14 @@ const JobApplications = () => {
               })}
             </Select>
           </FormControl>
-        </Box>
+        </Box> */}
 
         <RejectAllApplicationModal position={applicationData.position} />
       </Stack>
 
-      <Box sx={{ height: 400, width: "100%", overflowX: "auto" }}>
+      <Box sx={{ height: 400, width: "100%" }}>
         <DataGrid
-          rows={data?.applications}
+          rows={applications.length > 0 && applications}
           columns={columns}
           autoHeight
           autoHeightMax={400}
@@ -258,201 +266,36 @@ const JobApplications = () => {
             }
           }}
           sx={{
-            // "--DataGrid-overlayHeight": "300px",
+            "--DataGrid-overlayHeight": "250px",
             height: 400,
             width: "100%",
-            overflowX: "auto"
+            // boxShadow: 2,
+            // border: 1,
+            // borderColor: "primary.light",
+            // "& .MuiDataGrid-cell:hover": {
+            //   color: "primary.main"
+            // },
+            "& .warning": {
+              // border: 1,
+              // borderColor: "warning.main",
+              color: 'warning.main'
+            }
+          }}
+          getRowClassName={(params) => {
+            const applicationAnswers = params.row.ApplicationAnswers;
+
+            let bool = false;
+
+            for(const applicationAnswer of applicationAnswers){
+              if(!compareAnswer(applicationAnswer.answer, applicationAnswer.id)){
+                bool = true;
+              }
+            }
+
+            return bool && "warning";
           }}
         />
       </Box>
-
-      {/* <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead sx={{ backgroundColor: "background.paper" }}>
-            <TableRow>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", fontSize: 12 }}
-              >
-                Fullname
-              </TableCell>
-            
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", fontSize: 12 }}
-              >
-                Gender
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", fontSize: 12 }}
-              >
-                Nationality
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", fontSize: 12 }}
-              >
-                Id/Passport Number
-              </TableCell>
-              <TableCell align="center" sx={{ fontWeight: "bolder" }}>
-                Contact Number
-              </TableCell>
-              <TableCell align="center" sx={{ fontWeight: "bolder" }}>
-                Date Submitted
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", fontSize: 12 }}
-              >
-                Status
-              </TableCell>
-              <TableCell
-                align="center"
-                sx={{ fontWeight: "bolder", fontSize: 12 }}
-              >
-                Action
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {(rowsPerPage > 0
-              ? applications?.slice(
-                  page * rowsPerPage,
-                  page * rowsPerPage + rowsPerPage
-                )
-              : applications
-            )?.map((application, i) => {
-              return (
-                <TableRow key={application.id}>
-                  <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                    sx={{ fontSize: 12 }}
-                  >
-                    {application.fullname}
-                  </TableCell>
-                  
-                  <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                    sx={{ fontSize: 12 }}
-                  >
-                    {application.gender}
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                    sx={{ fontSize: 12 }}
-                  >
-                    {application.nationality}
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                    sx={{ fontSize: 12 }}
-                  >
-                    {application.idNumber}
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                    sx={{ fontSize: 12 }}
-                  >
-                    {application.cellphone}
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                    sx={{ fontSize: 12 }}
-                  >
-                    {application.createdAt}
-                  </TableCell>
-                  <TableCell
-                    align="center"
-                    component="th"
-                    scope="row"
-                    sx={{ fontSize: 12 }}
-                  >
-                    <Chip
-                      color={
-                        application.status === "submitted"
-                          ? "primary"
-                          : application.status === "shortlisted"
-                          ? "success"
-                          : "error"
-                      }
-                      label={
-                        application.status === "submitted"
-                          ? "submitted"
-                          : application.status === "shortlisted"
-                          ? "shortlisted"
-                          : "rejected"
-                      }
-                    />
-                  </TableCell>
-
-                  <TableCell align="center">
-                    <Stack
-                      direction="row"
-                      spacing={2}
-                      // border={1}
-                      justifyContent="center"
-                    >
-                      <Button
-                        variant="outlined"
-                        sx={{ fontSize: 10, width: 70 }}
-                        onClick={() => {
-                          navigate(
-                            `/humanResource/jobApplications/${positionId}/viewApplication/${application.id}`
-                          );
-                        }}
-                      >
-                        View
-                      </Button>
-                      <ShortListModal application={application} />
-                      <RejectApplicationModal application={application} />
-                    </Stack>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-          {applications?.length > 0 && (
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                  // colSpan={3}
-                  count={applications?.length || 0}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  SelectProps={{
-                    inputProps: {
-                      "aria-label": "rows per page"
-                    },
-                    native: true
-                  }}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                  ActionsComponent={TablePaginationActions}
-                />
-              </TableRow>
-            </TableFooter>
-          )}
-        </Table>
-        {(!applications || applications?.length === 0) && (
-          <Stack width="100%" padding={2}>
-            <CustomNoRowsOverlay description="No Application Made So far" />
-          </Stack>
-        )}
-      </TableContainer> */}
     </Stack>
   );
 };
